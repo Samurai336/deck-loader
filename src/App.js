2f6b9bf88a9a0b38fe0c13e229ec447eca75ec7d
samurai336 <samurai336@gmail.com>
2020-08-23 16:53:01 -0400

    better instructions

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ import { DecksContextProvider } from "./deckDataContext";
 function App() {
   return (
       <div className="App">
-      <DecksContextProvider>
-          <Uploader />
-          <DeckPack />
-      </DecksContextProvider>
+      <h1>Tiny Deck Rescue</h1>
+        <DecksContextProvider>
+            <Uploader />
+            <DeckPack />
+        </DecksContextProvider>
+        <div id="header">
+          <div id="header-content">
+            Want to contribute? Source code can be found <a href="https://github.com/Samurai336/DulingoDeckConverter">here</a>
+             and <a href="https://github.com/Samurai336/deck-loader"> here</a>. Pull requests welcome. 
+          </div>
+
+        </div>
       </div>
   );
 }
--- src/components/uploader.js
+++ src/components/uploader.js
@@ -4,7 +4,6 @@ import {ZipDecks} from "../converters/deckZipper"
 import React, {useState} from "react";
 
 
-
 export default function Uploader() { 
     const [deckFile, setDeckFile] = useState(null);
     const { deckDataState, setDeckData } = useDeckData();
@@ -37,29 +36,46 @@ export default function Uploader() {
     function fileData (){
         if(deckFile){
             return(
-            <div> 
-            <h2>File Details:</h2> 
-            <p>File Name: {deckFile.name}</p> 
-            <p>File Type: {deckFile.type}</p> 
-            <p> 
-              Last Modified:{" "} 
-              {deckFile.lastModifiedDate.toDateString()} 
-            </p> 
-            <button onClick={downloadAllDecks}>
+            <div max-width="400px" > 
+						<h3>
+								You can now preview your Tiny Card Decks Below. 
+							<br/>
+						</h3>
+							<h2>Step 3a</h2>
+							Click `Download All Decks` to download your deckdata and assets in a zip file.<br/>
+							The zip file, will contain each decks data as a JSON file and all your cards image resource will be in a folder called assets.
+							<br/>
+							<button onClick={downloadAllDecks}>
                 Download All Decks
-            </button>
+            	</button>
+							<h2>Step 3b</h2>
+							Download individual decks in a zip file by clicking `Download this Deck` next to each decks preview button.<br/>
+							The zip file, will contain that decks data as a JSON file and all its image resource will be in a folder called assets.
+							<br/>
           </div> 
             ); 
         }  
         return (
             <div> 
             <br /> 
-            <h4>Choose before Pressing the Upload button</h4> 
           </div> 
         )
     }
     return (
         <div>
+        <h2>Step 1</h2>
+            <p>
+                Go to <a href="https://support.duolingo.com/hc/en-us/articles/360043909772"> the shutdown FAQ </a> and follow the instructions under "What will happen to our existing decks?".
+            </p>
+            <p>
+                Following the instructions and use <a href="https://drive-thru.duolingo.com/">drive through</a> download your deck data. The file you need will be called `decks.csv`.
+            </p>
+        <h2>
+            Step 2
+        </h2>
+            <p>
+                Upload your decks.csv file here 
+            </p>
         <div> 
             <input type="file" onChange={(e) => onFileChange(e)} />
         </div>
